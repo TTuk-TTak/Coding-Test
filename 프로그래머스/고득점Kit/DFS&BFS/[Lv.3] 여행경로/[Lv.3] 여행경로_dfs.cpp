fcc6f51ec4726829cfe3ae601f5b64c82e4dfb98
YS <yyss241@gmail.com>
2023-04-09 17:08:00 +0900

    Create [Lv.3] 여행경로_dfs.cpp

---
+++ 프로그래머스/고득점Kit/DFS&BFS/[Lv.3] 여행경로/[Lv.3] 여행경로_dfs.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#define N 10000
+
+using namespace std;
+
+int TIKS;
+vector<vector<string>> TICKETS;
+vector<string> path;  // (TIKS+1)로 resize
+vector<int> visited;  // (TIKS)로 resize
+
+void recur(int lv, string city){
+    cout<<city<<"\n";
+    path[lv] = city;
+    // level
+    if(lv >= TIKS){
+        return;
+    }
+    // branch
+    for(int i=0; i<TIKS; i++){
+        if((city == TICKETS[i][0])&&(visited[i]==0)){
+            visited[i] = 1;
+            recur(lv+1, TICKETS[i][1]);
+            visited[i] = 0;
+        }
+    }
+}
+
+vector<string> solution(vector<vector<string>> tickets) {
+    TIKS = tickets.size();
+    TICKETS = tickets;
+    visited.resize(TIKS);       // resize 안하면 core dump out 됨 
+    path.resize(TIKS+1);
+    
+    recur(0, "ICN");
+
+    return path;
+}
